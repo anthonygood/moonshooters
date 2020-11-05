0e19ac11f372d5a800b3ef02749de337051ee74b
Anthony Good <good.anthony@gmail.com>
2020-11-05 17:34:26 +0000

    create some animations

--- src/scenes/Player.ts
+++ src/scenes/Player.ts
@@ -1,9 +1,12 @@
 const PLAYER_KEY = 'player';
 const asset = (path: string) => `/assets/${path}`;
 
+const RUN_VELOCITY = 250;
+const JUMP_VELOCITY = 500;
+
 class Player {
   public scene: Phaser.Scene;
-  public sprite: Phaser.GameObjects.Sprite;
+  public sprite: Phaser.Physics.Arcade.Sprite;
 
   constructor(scene) {
     this.scene = scene;
@@ -14,10 +17,29 @@ class Player {
   }
 
   create(): void {
-    // this.sprite = this.scene.add.sprite(150, 50, PLAYER_KEY, 'idle_1.png');
-    this.sprite = this.scene.physics.add.sprite(150, 50, PLAYER_KEY, 'idle_1.png');
+    const sprite = this.sprite = this.scene.physics.add.sprite(150, 50, PLAYER_KEY, 'idle_1.png');
+    sprite.debugShowVelocity = true;
 
-		this.sprite.setScale(4);
+    sprite.setScale(2);
+    this.scene.anims.create({
+      key: 'walk',
+      frameRate: 8,
+      repeat: -1,
+      frames: this.scene.anims.generateFrameNames(PLAYER_KEY, { suffix: '.png', prefix: 'walk_', start: 1, end: 4 }),
+    });
+    this.scene.anims.create({
+      key: 'idle',
+      frameRate: 2,
+      repeat: -1,
+      frames: this.scene.anims.generateFrameNames(PLAYER_KEY, { suffix: '.png', prefix: 'idle_', start: 1, end: 2 }),
+    });
+    this.scene.anims.create({
+      key: 'jump',
+      frameRate: 2,
+      repeat: -1,
+      frames: this.scene.anims.generateFrameNames(PLAYER_KEY, { suffix: '.png', prefix: 'jump_', start: 1, end: 2 }),
+    });
+    this.sprite.anims.play('jump');
   }
 
 	update(
@@ -25,19 +47,21 @@ class Player {
     delta: number,
     cursors: Phaser.Types.Input.Keyboard.CursorKeys
   ) {
-		this.sprite.setFrame('idle_1.png');
+    // this.sprite.anims.play('walk');
+    this.sprite.setVelocityX(0);
 		if (cursors.left.isDown) {
-			this.sprite.x -= 5;
+      this.sprite.setVelocityX(-RUN_VELOCITY);
 		}
 		if (cursors.right.isDown) {
-			this.sprite.x += 5;
-			this.sprite.setFrame('walk_1.png');
+      this.sprite.setVelocityX(RUN_VELOCITY);
+      // this.sprite.setFrame('walk_1.png');
+      // this.sprite.anims.play('walk');
 		}
 		if (cursors.down.isDown) {
 			this.sprite.y += 5;
 		}
 		if (cursors.up.isDown) {
-			this.sprite.y -= 5;
+      this.sprite.setVelocityY(-JUMP_VELOCITY);
 		}
 	}
 }
