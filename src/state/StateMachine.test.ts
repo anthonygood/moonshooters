3da81bde5816bdbbabf194daf142b8ab2ad7ff7c
Anthony Good <good.anthony@gmail.com>
2020-11-06 17:40:35 +0000

    more state machine functionality

--- src/state/StateMachine.test.ts
+++ src/state/StateMachine.test.ts
@@ -1,8 +1,8 @@
-import { StateMachine } from './PlayerState';
+import { StateMachine } from './StateMachine';
 
 describe('StateMachine', () => {
   it('returns a state machine that follows set of rules', () => {
-    const machine = StateMachine('idle')
+    const machine = StateMachine<any>('idle')
       .transitionTo('walk').when(data => data.key === '<walk>');
 
     expect(machine.currentState()).toBe('idle');
@@ -19,7 +19,7 @@ describe('StateMachine', () => {
   });
 
   it('works with more complex example', () => {
-    const machine = StateMachine('idle')
+    const machine = StateMachine<any>('idle')
       .transitionTo('walk').when(data => data.key === '<walk>').or(data => data.foo);
 
     expect(machine.currentState()).toBe('idle');
@@ -37,7 +37,7 @@ describe('StateMachine', () => {
   });
 
   it('works with yet more complex state graph example', () => {
-    const machine = StateMachine('idle')
+    const machine = StateMachine<any>('idle')
       .transitionTo('walking').when(data => data.walk || data.key === '<walk>')
       .state('walking')
       .transitionTo('jumping').when(data => data.jump).or(data => data.jump === '<jump>')
@@ -56,7 +56,53 @@ describe('StateMachine', () => {
     machine.process({ jump: true })
     expect(machine.currentState()).toBe('jumping');
 
+    // Can't transition to walking from jumping
+    machine.process({ key: '<walk>' })
+    expect(machine.currentState()).toBe('jumping');
+
     machine.process({ key: '<idle>' });
     expect(machine.currentState()).toBe('idle');
+  });
+
+  describe('initialising new state', () => {
+    it('calls function when transitionining to new state', () => {
+      const mock = jest.fn();
+      const machine = StateMachine<any>('idle')
+        .transitionTo('walk').when(data => data.walk).andThen(mock);
+
+      expect(mock).not.toHaveBeenCalled();
+
+      machine.process({ walk: true })
+
+      expect(mock).toHaveBeenCalled();
+    });
+
+    it('also works with the inverse construction', () => {
+      const mock = jest.fn();
+      const machine = StateMachine<any>('idle')
+        .transitionTo('walk').andThen(mock).when(data => data.walk);
+
+      expect(mock).not.toHaveBeenCalled();
+
+      machine.process({ walk: true })
+
+      expect(mock).toHaveBeenCalled();
+    });
+
+    it('initialises default state if needed', () => {
+      const init1 = jest.fn();
+      const init2 = jest.fn();
+      const machine = StateMachine<any>('idle').andThen(init1)
+        .transitionTo('walk').andThen(init2).when(data => data.walk);
+
+      expect(init1).not.toHaveBeenCalled()
+
+      machine.init();
+      expect(init1).toHaveBeenCalled()
+
+      expect(init2).not.toHaveBeenCalled();
+      machine.process({ walk: true })
+      expect(init2).toHaveBeenCalled();
+    });
   });
 });
---
+++ src/state/StateMachine.ts
@@ -0,0 +1,92 @@
+type State<Transition> = {
+  name: string;
+  transitions: Transition[];
+  init?: Function;
+}
+
+type StateDict<TData> = { [Key: string]: State<TData> }
+type Predicate<TData> = { (data: TData): boolean      }
+
+type PredicateTransition<TData> = {
+  predicate: Predicate<TData>,
+  state: string, // could be State rather than string?
+}
+
+export type TStateMachine<TData> = {
+  currentState: () => string;
+  transitionTo: (stateName: string) => TStateMachine<TData>;
+  when: (predicate: Predicate<TData>) => TStateMachine<TData>;
+  or: (predicate: Predicate<TData>) => TStateMachine<TData>;
+  andThen: (init: Function) => TStateMachine<TData>;
+  state: (stateName: string) => TStateMachine<TData>;
+  process: (data: TData) => TStateMachine<TData>;
+  init: () => TStateMachine<TData>;
+};
+
+export const StateMachine = <TData>(initialState: string): TStateMachine<TData> => {
+  type Transition = PredicateTransition<TData>;
+  const states: StateDict<Transition> = {
+    [initialState]: {
+      transitions: [],
+      name: initialState,
+    },
+  };
+
+  // states used by the monad when building state graph
+  let homeState = states[initialState];
+  let destState = homeState;
+  let currentStateName = initialState;
+
+  const machine: TStateMachine<TData> = {
+    transitionTo: stateName => {
+      if (stateName === homeState.name) {
+        throw new TypeError(`Cannot transition to same state: '${stateName}'`)
+      }
+
+      destState = states[stateName] = states[stateName] || {
+        name: stateName,
+        transitions: [],
+      };
+
+      return machine;
+    },
+    when: predicate => {
+      homeState.transitions.push({ predicate, state: destState.name });
+      return machine;
+    },
+    or: predicate => {
+      homeState.transitions.push({ predicate, state: destState.name });
+      return machine;
+    },
+    andThen: (fn: Function) => {
+      destState.init = fn;
+      return machine;
+    },
+    state: stateName => {
+      const nominatedState = states[stateName];
+      if (!nominatedState) {
+        throw new TypeError(`'${stateName}' not found in states: ${Object.keys(states)}`)
+      }
+      homeState = nominatedState;
+      return machine;
+    },
+    init: () => {
+      const { init } = states[currentStateName];
+      init();
+      return machine;
+    },
+    process: data => {
+      const transitions: PredicateTransition<TData>[] = states[currentStateName].transitions;
+      const transition = transitions.find(transition => transition.predicate(data));
+      if (transition) {
+        currentStateName = transition.state;
+        const { init } = states[currentStateName];
+        init && init();
+      }
+      return machine;
+    },
+    currentState: () => currentStateName,
+  };
+
+  return machine;
+};
