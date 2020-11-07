5f904cfa3f4fbd3a509dce967b91ddfdf929fb65
Anthony Good <good.anthony@gmail.com>
2020-11-07 16:19:09 +0000

    eliminate troublesome 'this' in Helpers

--- src/state/PlayerState.ts
+++ src/state/PlayerState.ts
@@ -1,22 +1,24 @@
 import { StateMachine, TStateMachine } from './StateMachine';
 
-const Helpers = ({ sprite, velocities }: PlayerState.Config) => ({
-  isOnGround: () => sprite.body.touching.down || sprite.body.blocked.down,
-  noDirectionPressed: (data: PlayerState.ProcessParams) => !data.cursors.right.isDown && !data.cursors.left.isDown && !data.cursors.up.isDown,
-  shouldJump(data: PlayerState.ProcessParams) { return data.cursors.up.isDown && this.isOnGround() },
-  leftOrRightIsPressed: (data: PlayerState.ProcessParams) => data.cursors.right.isDown || data.cursors.left.isDown,
-  onGroundAnd(fn: (any) => boolean) { return (data) => this.isOnGround() && fn(data) },
-  moveLeftRight: (data: PlayerState.ProcessParams) => {
-    const change = this.isOnGround() ? velocities.run : velocities.jump / 2;
-    let velocity = 0;
-    if (data.cursors.right.isDown) velocity += change;
-    if (data.cursors.left.isDown) velocity -= change;
-    this.sprite.setAccelerationX(velocity);
-  },
-});
+function Helpers({ sprite, velocities }: PlayerState.Config) {
+  const isOnGround = () => sprite.body.touching.down || sprite.body.blocked.down;
+  return {
+    isOnGround,
+    noDirectionPressed: (data: PlayerState.ProcessParams) => !data.cursors.right.isDown && !data.cursors.left.isDown && !data.cursors.up.isDown,
+    shouldJump: (data: PlayerState.ProcessParams) => data.cursors.up.isDown && isOnGround(),
+    leftOrRightIsPressed: (data: PlayerState.ProcessParams) => data.cursors.right.isDown || data.cursors.left.isDown,
+    onGroundAnd: (fn: (any) => boolean) => (data: PlayerState.ProcessParams) => isOnGround() && fn(data),
+    moveLeftRight: (data: PlayerState.ProcessParams) => {
+      const change = isOnGround() ? velocities.run : velocities.jump / 2;
+      let velocity = 0;
+      if (data.cursors.right.isDown) velocity += change;
+      if (data.cursors.left.isDown) velocity -= change;
+      sprite.setAccelerationX(velocity);
+    },
+  };
+};
 
 class PlayerState {
-  private sprite: Phaser.Physics.Arcade.Sprite;
   private direction: PlayerState.Machine;
   private action: PlayerState.Machine;
 
@@ -30,25 +32,26 @@ class PlayerState {
       .state('left').transitionTo('right').when(helpers.onGroundAnd(data => data.cursors.left.isDown))
       .andThen(() => sprite.flipX = false);
 
+    // Idle state
     this.action = <PlayerState.Machine>StateMachine('idle')
       .andThen(() => {
         sprite.anims.play('idle');
         sprite.setAccelerationX(0);
       })
-      .tick(() => this.sprite.setVelocityX(0))
+      .tick(() => sprite.setVelocityX(0))
       .transitionTo('jump').when(helpers.shouldJump)
       .transitionTo('walk').when(helpers.leftOrRightIsPressed)
 
-      .state('walk').andThen((_data: PlayerState.ProcessParams) => {
-        sprite.anims.play('walk');
-      })
+    // Walk state
+      .state('walk').andThen(() => sprite.anims.play('walk'))
       .tick(helpers.moveLeftRight)
       .transitionTo('idle').when(helpers.noDirectionPressed)
       .transitionTo('jump').when(helpers.shouldJump)
 
+    // Jump state
       .state('jump').andThen(() => {
         sprite.setVelocityY(-velocities.jump);
-        this.sprite.anims.play('jump', true);
+        sprite.anims.play('jump', true);
       })
       .tick(helpers.moveLeftRight)
       .transitionTo('idle').when(helpers.onGroundAnd(helpers.noDirectionPressed))
