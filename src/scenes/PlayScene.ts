248a4fc95696699a113f8e9d1ecf873fde6c9e95
Anthony Good <good.anthony@gmail.com>
2020-11-15 23:44:11 +0000

    WIP play scene changes

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,8 +1,9 @@
-import Player from '../entities/Player';
+import Player from '../entities/NPC';
 import Fog from '../entities/Fog';
 
 const asset = (path: string) => `/assets/${path}`;
 
+const MAP_SCALE = 1.5;
 const MAP_KEY = 'map';
 const LEVEL_KEY = 'level';
 const BKG_KEY = 'background';
@@ -23,40 +24,37 @@ class TestScene extends Phaser.Scene {
 	preload() {
 		this.load.tilemapTiledJSON(MAP_KEY, asset('tilemaps/Test Map.json'));
 		this.load.image(LEVEL_KEY, asset('tilemaps/platforms_extruded.png'));
-		this.load.image(BKG_KEY, asset('tilemaps/skyscraper_tiles_extruded.png'));
+		this.load.image(BKG_KEY, asset('tilemaps/skyscraper_tiles_extruded.grey.png'));
 		this.load.image(SKY_KEY, 'assets/sky.png');
 		this.player.preload();
 	}
 
 	create() {
-		const sky = this.add.image(0, 0, SKY_KEY).setOrigin(0, 0);
+		// const sky = this.add.image(0, 0, SKY_KEY).setOrigin(0, 0);
 		const map = this.map = this.make.tilemap({ key: MAP_KEY });
 		const tileset = map.addTilesetImage('Platforms', LEVEL_KEY);
 		const scrapers = map.addTilesetImage('Skyscrapers', BKG_KEY);
-		const distantBackground = map.createStaticLayer('Back scrapers', scrapers, 0, -200);
-		new Fog(this).add('fog1');
-		const midBackground = map.createStaticLayer('Scrapers', scrapers, 0, -200);
+		const distantBackground = map.createStaticLayer('Back scrapers', scrapers, 0, 0);
+		// new Fog(this).add('fog1');
+		const midBackground = map.createStaticLayer('Scrapers', scrapers, 0, 0);
 		new Fog(this).add('fog2');
 
-
 		midBackground.scrollFactorX = 0.3;
-		midBackground.scrollFactorY = 0.3;
+		midBackground.scrollFactorY = 0.9;
 		distantBackground.scrollFactorX = 0.2;
-		distantBackground.scrollFactorY = 0.2;
+		distantBackground.scrollFactorY = 0.8;
 
 		const layer = map.createStaticLayer('World', tileset);
 		layer.setCollisionByProperty({ collides: true });
-
-		const skyScale = map.heightInPixels / sky.height;
-		sky.setScale(skyScale).setScrollFactor(0);
+		midBackground.scale = layer.scale = distantBackground.scale = MAP_SCALE;
 
 		this.cursors = this.input.keyboard.createCursorKeys();
 
 		this.player.create();
-		this.physics.add.collider(this.player.sprite, layer);
+		this.physics.add.collider(this.player.container, layer);
 
-		this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-		this.cameras.main.startFollow(this.player.sprite, false);
+		this.cameras.main.setBounds(0, 0, map.widthInPixels * MAP_SCALE, map.heightInPixels * MAP_SCALE);
+		this.cameras.main.startFollow(this.player.container, false);
 
 		// debug
 		window.game = this;
@@ -72,9 +70,9 @@ class TestScene extends Phaser.Scene {
 	}
 
 	update(time: number, delta: number) {
-		if (this.player.sprite.body.top > this.map.heightInPixels) {
-			// respawn
-			this.player.sprite.setPosition(this.player.spawn[0], 0);
+		if (this.player.container.body.top > this.map.heightInPixels * MAP_SCALE) {
+		// respawn
+			this.player.container.setPosition(this.player.spawn[0], 0);
 		}
 		this.player.update(time, delta, this.cursors);
 	}
