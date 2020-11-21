e4a636e362298465ac73ca5af3d5e37b845ba5f0
Anthony Good <good.anthony@gmail.com>
2020-11-21 20:53:15 +0000

    simpler working version for swapping tile images

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -9,7 +9,6 @@ const asset = (path: string) => `/assets/${path}`;
 const MAP_SCALE = 1.5;
 const MAP_KEY = 'map';
 const LEVEL_KEY = 'level';
-const BKG_KEY = 'background';
 const DEFAULT_BKG_KEY = 'default-background-scrapers';
 const GREY_BKG_KEY = 'grey-background-scrapers';
 const TURQUOISE_BKG_KEY = 'turqouise-background-scrapers';
@@ -18,28 +17,11 @@ const PINK_BKG_KEY = 'pink-background-scrapers';
 const GREEN_BKG_KEY = 'green-background-scrapers';
 
 const SPAWN_RATE = 250;
-const PINK_KEY = 'pink';
 
-// The index offset for avoiding collisions when generating dynamic tile IDs.
-const TILESET_OFFSET = 136;
-
-const CITY_TILES = {
-	Platforms: {
-		imageName: 'Platforms',
-		layers: {
-			World: 'World',
-		},
-	},
-	Skyscrapers: {
-		imageName: 'Skyscrapes',
-		layers: {
-			Scrapers: 'Scrapers',
-			'Back scrapers': 'Back scrapers',
-			'Right back': 'Right back',
-		},
-	}
-};
-
+// Create additional layers that just alias the background layer.
+// Everything is the same (including tile GIDS) except the name.
+// The different name allows you to add a different tileset image
+// for the same layer data, so you can apply a 'theme' per layer.
 const addAlternativeCityTilesets = (
 	json,
 	altCount = 1
@@ -47,73 +29,11 @@ const addAlternativeCityTilesets = (
 	const [firstSet] = json.tilesets;
 
 	for (let i = 1; i <= altCount; i++) {
-		const firstgid = TILESET_OFFSET;
 		json.tilesets.push({
 			...firstSet,
 			name: `Skyscrapers_alt_${i}`,
-			firstgid
 		});
 	}
-};
-
-const addAlternateCityTilemapIds = (json, altCount = 1) => {
-	const newLayers = [];
-	const layerIdOffset = json.layers.length;
-	json.layers.forEach(layer => {
-		const { name, id, data, ...rest } = layer;
-		if (layer.name === 'World') return;
-
-		console.log('adding extra lyer', layer.name);
-		const memo = {};
-		const newLayerData = layer.data.map(id => {
-			const firstgid = TILESET_OFFSET;
-
-			if (id && id <= 126) {
-				memo[id] = id + firstgid - 1;
-				// console.log(`${id} becomes ${id + firstgid}`);
-				return id + firstgid - 1;
-			}
-			return id;
-		});
-
-		console.log('newLayerData', memo);
-
-		newLayers.push({
-			...rest,
-			name: `${name}_alt_${1}`,
-			data: newLayerData,
-			id: id + layerIdOffset - 1,
-		});
-
-		// for (let i = 1; i <= altCount; i++) {
-		// 	console.log('adding extra lyer');
-		// 	const memo = {}
-		// 	const newLayerData = layer.data.map(id => {
-		// 		const firstgid = TILESET_OFFSET * i;
-
-		// 		if (id >= 1 && id <= 126) {
-		// 			memo[id] = id + firstgid;
-		// 			// console.log(`${id} becomes ${id + firstgid}`);
-		// 			return id + firstgid;
-		// 		}
-		// 		return id;
-		// 	});
-
-		// 	console.log('newLayerData', memo, layer.name, newLayerData.toString());
-
-		// 	newLayers.push({
-		// 		...rest,
-		// 		name: `${name}_alt_${i}`,
-		// 		data: newLayerData,
-		// 		id: id + layerIdOffset - 1,
-		// 	});
-		// }
-	});
-
-	json.nextlayerid = json.layers.length + 1;
-
-	json.layers.push(...newLayers);
-	console.log('new json', json);
 };
 
 class TestScene extends Phaser.Scene {
@@ -133,8 +53,7 @@ class TestScene extends Phaser.Scene {
 	}
 
 	preload() {
-		addAlternativeCityTilesets(json, 4);
-		addAlternateCityTilemapIds(json, 4);
+		addAlternativeCityTilesets(json, 5);
 		this.load.tilemapTiledJSON(MAP_KEY, json);
 		this.load.image(LEVEL_KEY, asset('tilemaps/platforms_extruded.png'));
 
@@ -143,6 +62,7 @@ class TestScene extends Phaser.Scene {
 		this.load.image(PINK_BKG_KEY, asset('tilemaps/skyscraper_tiles_extruded.pink.png'));
 		this.load.image(YELLOW_BKG_KEY, asset('tilemaps/skyscraper_tiles_extruded.yellow.png'));
 		this.load.image(GREEN_BKG_KEY, asset('tilemaps/skyscraper_tiles_extruded.green.png'));
+		this.load.image(GREY_BKG_KEY, asset('tilemaps/skyscraper_tiles_extruded.grey.png'));
 
 		this.player.preload();
 		this.NPCs.forEach(npc => npc.preload())
@@ -158,12 +78,13 @@ class TestScene extends Phaser.Scene {
 		const yellow = map.addTilesetImage('Skyscrapers_alt_2', YELLOW_BKG_KEY);
 		const turqouise = map.addTilesetImage('Skyscrapers_alt_3', TURQUOISE_BKG_KEY);
 		const pink = map.addTilesetImage('Skyscrapers_alt_4', PINK_BKG_KEY);
+		const grey = map.addTilesetImage('Skyscrapers_alt_5', GREY_BKG_KEY);
 
 		// Apply alternate skyscraper tiles (or not)
-		const wayBackground = map.createStaticLayer('Right back_alt_1', pink, 0, 0).setDepth(1);
-		const distantBackground = map.createStaticLayer('Back scrapers_alt_1', green, 0, 0).setDepth(3);
-		const midBackground = map.createStaticLayer('Scrapers_alt_1', turqouise, 0, 0).setDepth(5);
-		// Fog.night(this);
+		const wayBackground = map.createStaticLayer('Right back', pink, 0, 0).setDepth(1);
+		const distantBackground = map.createStaticLayer('Back scrapers', pink, 0, 0).setDepth(3);
+		const midBackground = map.createStaticLayer('Scrapers', turqouise, 0, 0).setDepth(5);
+		Fog.sunset(this);
 
 		midBackground.scrollFactorX = 0.3;
 		midBackground.scrollFactorY = 0.9;
