745b2b727ad549a062a523bfe0e48490b83ad067
Anthony Good <good.anthony@gmail.com>
2020-11-22 22:24:10 +0000

    extract NPCColours

--- src/entities/NPC.ts
+++ src/entities/NPC.ts
@@ -1,3 +1,4 @@
+import COLOURS from './NPCColours';
 import { Direction } from '../state/Direction';
 import Player, { SpriteLayer, VELOCITY } from './Player';
 import NPCState from '../state/NPCState';
@@ -46,40 +47,6 @@ const Driver: () => NPCDriver = () => {
     go,
     change,
   };
-};
-
-const COLOURS = {
-  white: 0xF7F7F7,
-  grey: 0x646464,
-  black: 0x000000,
-  green: 0x89F94F,
-  blue: 0x0376BB,
-  shirt: {
-    blue: 0x74FBEA,
-    yellow: 0xFFFC67,
-    pink: 0xFFD7EB,
-  },
-  skin: {
-    pale: 0xFDDCD4,
-    pale2: 0xfaefdc,
-    pink: 0xFFC0B0,
-    tan: 0xFFDA8C,
-    brown: 0xCF7408,
-  },
-  hair: {
-    white: 0xD5D5D5,
-    ginger: 0xFF9400,
-    brown: 0x4A2C02,
-    black: 0x2B2A2A,
-  },
-  mask: {
-    blue: 0x4FE1FC,
-    black: 0x292727,
-  },
-  tie: {
-    red: 0xB51700,
-    blue: 0x02578A,
-  },
 };
 
 class NPC extends Player {
---
+++ src/entities/NPCColours.ts
@@ -0,0 +1,36 @@
+
+const COLOURS = {
+  white: 0xF7F7F7,
+  grey: 0x646464,
+  black: 0x000000,
+  green: 0x89F94F,
+  blue: 0x0376BB,
+  shirt: {
+    blue: 0x74FBEA,
+    yellow: 0xFFFC67,
+    pink: 0xFFD7EB,
+  },
+  skin: {
+    pale: 0xFDDCD4,
+    pale2: 0xfaefdc,
+    pink: 0xFFC0B0,
+    tan: 0xFFDA8C,
+    brown: 0xCF7408,
+  },
+  hair: {
+    white: 0xD5D5D5,
+    ginger: 0xFF9400,
+    brown: 0x4A2C02,
+    black: 0x2B2A2A,
+  },
+  mask: {
+    blue: 0x4FE1FC,
+    black: 0x292727,
+  },
+  tie: {
+    red: 0xB51700,
+    blue: 0x02578A,
+  },
+};
+
+export default COLOURS;
