31780cd1fcd43f00cf588186b0daf98aa6e2e3bd
Anthony Good <good.anthony@gmail.com>
2020-11-29 12:13:49 +0000

    add Van entity; tweak to level 3;

---
+++ src/entities/Van.ts
@@ -0,0 +1,69 @@
+import { NPCDirection } from './NPC/Driver';
+
+const VAN_KEY = 'van';
+const VELOCITY = 500;
+
+class Van {
+  public container: Phaser.GameObjects.GameObject;
+  private scene: Phaser.Scene;
+  private spawn: [number, number];
+  private boundary: number;
+  private direction: NPCDirection;
+
+  static preload(scene) {
+		scene.load.image(VAN_KEY, '/assets/sprites/white van.png');
+  }
+
+  constructor(scene) {
+    this.scene = scene;
+  }
+
+  create(x: number, y: number, boundary: number) {
+    this.spawn = [x, y];
+    this.boundary = boundary;
+    this.direction = boundary < x ? NPCDirection.Left : NPCDirection.Right;
+
+    if (this.container) this.container.destroy();
+
+    const { scene } = this;
+		const container = this.container = scene.add.container(x, y);
+
+		const sprite = scene.add.sprite(96, 48, VAN_KEY)
+			.setName(VAN_KEY)
+      .setScale(3);
+
+    let velocity = VELOCITY
+
+    // move to state
+    if (this.direction === NPCDirection.Left) {
+      sprite.flipX = true;
+      velocity = -velocity;
+    }
+
+    container.add(sprite).setDepth(5);
+
+    scene.physics.world.enable(container);
+    (container.body as Phaser.Physics.Arcade.Body)
+      .setGravity(0, -2500)
+      .setVelocityX(velocity)
+      .setSize(192, 128);
+  }
+
+  update() {
+    const { boundary, container, direction, spawn: [spawnX, spawnY] } = this;
+    const { body } = container;
+
+    // Determine which 'direction' to perform boundary check for,
+    // according to which direction the body is moving.
+    const beyondBounds = direction === NPCDirection.Left ?
+      body.x < boundary :
+      body.x > boundary;
+
+		if (beyondBounds) {
+      body.x = spawnX;
+      body.y = spawnY;
+    }
+  }
+}
+
+export default Van;
--- src/scenes/TheCity.ts
+++ src/scenes/TheCity.ts
@@ -6,12 +6,15 @@ import NPC, { Modifiers } from '../entities/NPC/NPC';
 import { Morning as Background } from '../entities/Background';
 import * as json from '../../assets/tilemaps/The City.json';
 import { NPCDirection } from '../entities/NPC/Driver';
+import Van from '../entities/Van';
 
-const asset = (path: string) => `/assets/${path}`;
 
 const MAP_SCALE = 1.5;
-// const MAP_KEY = 'TheCity';
 const LEVEL_KEY = 'level';
+const VAN_KEY = 'van';
+
+const asset = (path: string) => `/assets/${path}`;
+const mapScale = num => num * MAP_SCALE;
 
 const getModifiersFromProps = (properties) => {
 	const modifier = properties.find(({ name }) => name === 'modifier');
@@ -27,12 +30,20 @@ const getModifiersFromProps = (properties) => {
 	};
 }
 
+const id = properites => properites.find(prop => prop.name === 'id').value;
+
+const getBoundaryForVan = (map: Phaser.Tilemaps.Tilemap, van) => {
+	const vanId = id(van.properties);
+	return map.findObject('Objects', obj => obj.name === 'VanBoundary' && id(obj.properties) === vanId);
+}
+
 class TheCity extends Phaser.Scene {
 	private score: Score;
 	public player: Player;
 	public cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 	public map: Phaser.Tilemaps.Tilemap;
 	public NPCs: NPC[];
+	public van: Van;
 	public background: Background;
 	private disposeReport: () => void;
 
@@ -42,6 +53,7 @@ class TheCity extends Phaser.Scene {
 		this.background = this.getBackground();
 		this.score = new Score(this);
 		this.NPCs = [];
+		this.van = new Van(this);
 	}
 
   getBackground() {
@@ -49,7 +61,6 @@ class TheCity extends Phaser.Scene {
   }
 
   getLevelJson() {
-		console.log('TheCity.getLevelJson');
     return json;
 	}
 
@@ -63,8 +74,11 @@ class TheCity extends Phaser.Scene {
 		this.load.tilemapTiledJSON(this.getMapKey(), json);
 		this.load.image(LEVEL_KEY, asset('tilemaps/platforms_extruded.png'));
 
+		this.load.image(VAN_KEY, asset('sprites/white van.png'));
+
 		this.player.preload();
 		NPC.preload(this);
+		Van.preload(this);
 	}
 
 	create() {
@@ -72,7 +86,7 @@ class TheCity extends Phaser.Scene {
 		const tileset = map.addTilesetImage('Platforms', LEVEL_KEY);
 		this.background.create(map, MAP_SCALE);
 
-		const layer = map.createStaticLayer('World', tileset).setDepth(5);
+		const layer = map.createStaticLayer('World', tileset).setDepth(6);
 		layer.setCollisionByProperty({ collides: true });
 
 		layer.scale = MAP_SCALE;
@@ -81,7 +95,7 @@ class TheCity extends Phaser.Scene {
 
 		const playerSpawn = this.playerSpawn = map.findObject('Objects', obj => obj.name === 'PlayerSpawn');
 		this.player.create(this.cursors, [playerSpawn.x * MAP_SCALE, playerSpawn.y * MAP_SCALE]);
-		this.player.container.setDepth(6);
+		this.player.container.setDepth(7);
 		this.physics.add.collider(this.player.container, layer);
 
 		this.cameras.main.setBounds(0, 0, map.widthInPixels * MAP_SCALE, map.heightInPixels * MAP_SCALE);
@@ -91,7 +105,7 @@ class TheCity extends Phaser.Scene {
 		this.score.create();
 
 		this.NPCs.forEach(npc => {
-			this.physics.add.overlap(npc.container, this.player.container, (npcContainer, player) => {
+			this.physics.add.overlap(npc.container, this.player.container, (npcContainer, _player) => {
 				!npcContainer.getData('touchedByPlayer') && this.score.increment();
 				npcContainer.setData('touchedByPlayer', true);
 
@@ -101,43 +115,54 @@ class TheCity extends Phaser.Scene {
 			});
 		});
 
+		const van = this.map.findObject('Objects', obj => obj.name === 'VanSpawn');
+		const boundary = getBoundaryForVan(map, van);
+		this.van.create(mapScale(van.x), mapScale(van.y - 10), mapScale(boundary.x));
+
+		this.physics.add.overlap(this.van.container, this.player.container, (_van, player) => {
+			if (player.getData('roadkill')) return;
+			player.setData('roadkill', true);
+			// this.player.container.setRotation(-1.5);
+			// this.player.container.body.setSize(
+			// 	this.player.container.body.height,
+			// 	2
+			// ).setVelocityX(-1000);
+			this.end();
+		});
+
 		// debug
 		window.game = this;
-		window.game.layer = layer;
-		// const ladderIndices = layer.filterTiles(_ => _.properties.climbable).map(_ => _.index);
-		// layer.setTileIndexCallback(ladderIndices, this.player.nearClimbable, this.player)
-		// const debugGraphics = this.add.graphics().setAlpha(.75);
-		// layer.renderDebug(debugGraphics, {
-		// 	tileColor: null, // Colour of non-colliding tiles
-		// 	collidingTileColor: new Phaser.Display.Color(255, 255, 255, 255), // Color of colliding tiles
-		// 	faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-		// });
 	}
 
 	update(time: number, delta: number) {
     const fallenBelowBounds = this.player.container.body.top > this.map.heightInPixels * MAP_SCALE;
-		if (fallenBelowBounds) {
-      if (!this.score.end) {
-				this.score.finish();
-				const ratings = rate(this.score, this.player.state.flightRecorder);
-				this.disposeReport = EndLevelReport(this, this.score, ratings.join('\n'));
-      } else {
-        if (this.cursors.space.isDown) {
-					this.player.destroy();
-					this.disposeReport();
-					this.NPCs.forEach(npc => npc.destroy());
-					this.NPCs = [];
-					this.score.pass ? this.nextScene() : this.scene.restart();
-        }
-      }
+		if (fallenBelowBounds || this.score.end) {
+			if (!this.score.end) this.end();
+			if (this.cursors.space.isDown) this.restart();
 		} else {
 			this.player.update(time, delta);
 		}
+
+		this.van.update();
 
 		// TODO: only update NPCs nearby?
 		this.NPCs
 			.filter(npc => npc.spawned)
 			.forEach(npc => npc.update(time, delta));
+	}
+
+	end() {
+		this.score.finish();
+		const ratings = rate(this.score, this.player.state.flightRecorder);
+		this.disposeReport = EndLevelReport(this, this.score, ratings.join('\n'));
+	}
+
+	restart() {
+		this.player.destroy();
+		this.disposeReport();
+		this.NPCs.forEach(npc => npc.destroy());
+		this.NPCs = [];
+		this.score.pass ? this.nextScene() : this.scene.restart();
 	}
 
 	spawnNPCs(layer: Phaser.Tilemaps.StaticTilemapLayer) {
@@ -153,9 +178,20 @@ class TheCity extends Phaser.Scene {
 				const npc = new NPC(this);
 				const modifiers = getModifiersFromProps(properties);
 				npc.create(cursors, [x * MAP_SCALE, y * MAP_SCALE], modifiers);
-				npc.container.setDepth((npcCount % 2) + 5);
+				npc.container.setDepth((npcCount % 2) + 6);
 				physics.add.collider(npc.container, layer);
 				NPCs.push(npc);
+		});
+	}
+
+	debug(worldLayer) {
+		const ladderIndices = worldLayer.filterTiles(_ => _.properties.climbable).map(_ => _.index);
+		worldLayer.setTileIndexCallback(ladderIndices, this.player.nearClimbable, this.player)
+		const debugGraphics = this.add.graphics().setAlpha(.75);
+		worldLayer.renderDebug(debugGraphics, {
+			tileColor: null, // Colour of non-colliding tiles
+			collidingTileColor: new Phaser.Display.Color(255, 255, 255, 255), // Color of colliding tiles
+			faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
 		});
 	}
 
