4b9ebd0a978df24fbb0a521db4235ef4df13b4ef
Anthony Good <good.anthony@gmail.com>
2020-11-30 21:54:13 +0000

    more and more longhand

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 webpack --config webpack.production.config.js
 mkdir -p build/assets/sprites build/assets/tilemaps
 cp assets/sprites/*png build/assets/sprites
-cp assets/tilemaps/{*png,*json} build/assets/tilemaps
+cp assets/tilemaps/*png build/assets/tilemaps
+cp assets/tilemaps/*json build/assets/tilemaps
