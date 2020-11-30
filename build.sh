1af5c0098ad4896d0d576068bfe5d4277245d85e
Anthony Good <good.anthony@gmail.com>
2020-11-30 21:43:49 +0000

    try tinkering with build.sh directories

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 webpack --config webpack.production.config.js
-mkdir -p ./build/assets/{sprites,tilemaps}
-cp ./assets/sprites/*png ./build/assets/sprites
-cp ./assets/tilemaps/{*png,*json} ./build/assets/tilemaps
+mkdir -p build/assets/{sprites,tilemaps}
+cp assets/sprites/*png build/assets/sprites
+cp assets/tilemaps/{*png,*json} build/assets/tilemaps
