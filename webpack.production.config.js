a4f858aeee6fa224861187c4899c6dec7c4e03a6
Anthony Good <good.anthony@gmail.com>
2020-11-30 21:04:06 +0000

    upgrade typescript and add typecheck script

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -8,8 +8,8 @@ var phaser = path.join(phaserModule, 'src/phaser.js')
 
 var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false')),
-  WEBGL_RENDERER: true, 
-  CANVAS_RENDERER: true 
+  WEBGL_RENDERER: true,
+  CANVAS_RENDERER: true
 })
 
 module.exports = {
@@ -60,9 +60,9 @@ module.exports = {
         loaders: ['babel-loader', 'awesome-typescript-loader'],
         include: path.join(__dirname, 'src'),
       },
-      { 
-        test: [/\.vert$/, /\.frag$/], 
-        use: 'raw-loader' 
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader'
       }
     ]
   },
