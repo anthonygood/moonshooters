b0fa3d8d38ec1133beb34416b52636d3d035ad48
Anthony Good <good.anthony@gmail.com>
2021-01-09 21:43:39 +0000

    update build.sh for sound assets

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 webpack --config webpack.production.config.js
 mkdir -p build/assets/sprites build/assets/tilemaps
+mkdir -p build/assets/sound/boris
 cp assets/sprites/*png build/assets/sprites
 cp assets/tilemaps/*png build/assets/tilemaps
 cp assets/tilemaps/*json build/assets/tilemaps
+cp assets/sound/*mp3 build/assets/sound
+cp assets/sound/boris/*m4a build/assets/sound/boris
