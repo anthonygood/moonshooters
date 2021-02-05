10f3121b0665ae743da9358678689782fab0f85f
Anthony Good <good.anthony@gmail.com>
2021-02-05 21:41:53 +0000

    upto

--- src/util/index.ts
+++ src/util/index.ts
@@ -3,5 +3,8 @@ export * as DynamicAtlas from './dynamicSpriteAtlas';
 
 export const asset = (path: string) => `./assets/${path}`;
 
+// Return a whole number between 1 and val, inclusive
+export const upto = (val: number) => 1 + Math.floor(Math.random() * val);
+
 export const sample = (vals = []) =>
   vals[Math.floor(Math.random() * vals.length)];
