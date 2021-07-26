aa25644c3039198368813ebe1a6ecee4b1ca465f
Anthony Good <good.anthony@gmail.com>
2021-07-26 12:13:45 +0100

    use glsl tagged template for GreyscalePipeline

--- src/rendering/GreyscalePipeline.ts
+++ src/rendering/GreyscalePipeline.ts
@@ -1,22 +1,27 @@
 import Phaser from 'phaser';
 
+// workaround for using glsl tagged templates
+const glsl = strings => strings[0];
+
+const fragShader = glsl`
+  precision mediump float;
+  uniform sampler2D uMainSampler;
+  varying vec2 outTexCoord;
+  void main(void) {
+    vec4 color = texture2D(uMainSampler, outTexCoord);
+    float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
+    gl_FragColor = vec4(vec3(gray), color.a);
+  }
+`;
+
 // Copied from https://www.stephengarside.co.uk/blog/phaser-3-black-and-white-or-greyscale-sprites/
 const GreyscalePipeline = new Phaser.Class({
   Extends: Phaser.Renderer.WebGL.Pipelines.SinglePipeline,
   initialize: function GrayscalePipeline(game) {
-    console.log('initialise', game);
     Phaser.Renderer.WebGL.Pipelines.SinglePipeline.call(this, {
       game: game,
       renderer: game.renderer,
-      fragShader: `
-      precision mediump float;
-      uniform sampler2D uMainSampler;
-      varying vec2 outTexCoord;
-      void main(void) {
-      vec4 color = texture2D(uMainSampler, outTexCoord);
-      float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
-      gl_FragColor = vec4(vec3(gray), color.a);
-      }`
+      fragShader,
     });
   },
 });
